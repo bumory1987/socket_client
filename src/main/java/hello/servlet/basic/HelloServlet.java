b308106ab9360f30461a31fb394fab049ac11f1a
sungbum <bumory1987@gmail.com>
2022-07-21 11:20:59 +0900

    basic setting for mvc

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name= "helloServlet", urlPatterns = "/hello")
+@Slf4j
+public class HelloServlet extends HttpServlet {
+
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("Hello Friend!");
+//        System.out.println("request = " + request);
+//        System.out.println("response = " + response);
+
+        log.info("username = {}", request.getParameter("username"));
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello "+request.getParameter("username"));
+    }
+}
---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic.response;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseHtmlServlet" , urlPatterns = "/response-html" )
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write("<html>");
+        resp.getWriter().write("<body>");
+        resp.getWriter().write("<div>");
+        resp.getWriter().write("안녕? 친구여");
+        resp.getWriter().write("</div>");
+        resp.getWriter().write("</body>");
+        resp.getWriter().write("</html>");
+
+    }
+}
+
+
